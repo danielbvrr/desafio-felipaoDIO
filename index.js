715e30ca7ece9a4780f29d56f51058c779b9982a
danielbvrr <danielbvrr@gmail.com>
2026-03-18 11:58:51 -0400

    primeiro desafio

---
+++ index.js
@@ -0,0 +1,31 @@
+let herois = [
+    { nome: "Daniel", xp: 7500 },
+    { nome: "Ana", xp: 1200 },
+    { nome: "Carlos", xp: 10500 }
+];
+
+for (let i = 0; i < herois.length; i++) {
+    let nome = herois[i].nome;
+    let xp = herois[i].xp;
+    let nivel = "";
+
+    if (xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp <= 8000) {
+        nivel = "Platina";
+    } else if (xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp <= 10000) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Radiante";
+    }
+
+    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+}
